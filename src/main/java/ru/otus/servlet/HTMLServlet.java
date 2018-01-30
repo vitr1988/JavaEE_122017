9ec3b0dcd03c05219415668b4f4f398fa6900939
ViTr <ViTr@list.ru>
2018-01-30 22:31:59 +0400

    add(JavaEE_122017): Знакомство с GWT

---
+++ src/main/java/ru/otus/servlet/HTMLServlet.java
@@ -0,0 +1,20 @@
+package ru.otus.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/servlet")
+public class HTMLServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/page.html").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/page.html").forward(request, response);
+    }
+}
