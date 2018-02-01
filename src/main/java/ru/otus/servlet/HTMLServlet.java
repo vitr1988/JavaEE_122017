9a24aad6e91aa6f8b5f34d221506fd9b6e60a8e7
Vitalii Ivanov <vitr.ivanov@gmail.com>
2018-02-01 17:35:23 +0400

    feat (JavaEE_122017): Server Push

--- src/main/java/ru/otus/servlet/HTMLServlet.java
+++ src/main/java/ru/otus/servlet/HTMLServlet.java
@@ -5,12 +5,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.PushBuilder;
 import java.io.IOException;
 
 @WebServlet("/servlet")
 public class HTMLServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final PushBuilder pushBuilder = request.newPushBuilder();
+        if (pushBuilder != null) {
+            pushBuilder.path("images/hotnews.png").push();
+        }
         request.getRequestDispatcher("/page.html").forward(request, response);
     }
 
