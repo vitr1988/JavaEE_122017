104dfba58b8e11fa477648b3e03bf5d7e55273fa
ViTr <ViTr@list.ru>
2018-03-02 22:07:05 +0400

    add(JavaEE_122017): WebSocket API

---
+++ src/main/java/ru/otus/websocket/WebSocketContextListener.java
@@ -0,0 +1,75 @@
+package ru.otus.websocket;
+
+import ru.otus.websocket.RateServerEndpoint;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.websocket.DeploymentException;
+import javax.websocket.server.ServerContainer;
+
+@WebListener
+public class WebSocketContextListener implements ServletContextListener {
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+      /* This method is called when the servlet context is
+         initialized(when the Web application is deployed). 
+         You can initialize servlet context related data here.
+      */
+        ServletContext servletContext = sce.getServletContext();
+        ServerContainer container = (ServerContainer) servletContext.getAttribute("javax.websocket.server.ServerContainer");
+//        try {
+//            container.addEndpoint(RateServerEndpoint.class);
+//        } catch (DeploymentException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionListener implementation
+    // -------------------------------------------------------
+    public void sessionCreated(HttpSessionEvent se) {
+        /* Session is created. */
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se) {
+        /* Session is destroyed. */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionAttributeListener implementation
+    // -------------------------------------------------------
+
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute 
+         is added to a session.
+      */
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is removed from a session.
+      */
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+      /* This method is invoked when an attibute
+         is replaced in a session.
+      */
+    }
+}
