ef4eab3bc787aebc6686d5b1238a93ffe9f21b8c
Vitalii Ivanov <vitr.ivanov@gmail.com>
2018-03-21 16:16:23 +0400

    add (JavaEE_122017): JAX-WS

---
+++ src/main/java/ru/otus/jaxws/UserService.java
@@ -0,0 +1,15 @@
+package ru.otus.jaxws;
+
+import ru.otus.jaxrs.model.User;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.DOCUMENT)
+public interface UserService {
+    @WebMethod
+    User getUserById(int id);
+}
---
+++ src/main/java/ru/otus/jaxws/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package ru.otus.jaxws;
+
+import ru.otus.jaxrs.model.User;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = " ru.otus.jaxws.UserService")
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User getUserById(int id) {
+        return new User();
+    }
+}
