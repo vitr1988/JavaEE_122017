884adcea378afb72a4ffbc34b07f15c724eabf77
Vitalii Ivanov <vitr.ivanov@gmail.com>
2018-03-26 16:17:55 +0400

    add (JavaEE_122017): Create Proxy example

---
+++ src/main/java/ru/otus/ejb/session/proxy/TransactionFactory.java
@@ -0,0 +1,41 @@
+package ru.otus.ejb.session.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class TransactionFactory {
+
+    private static class TransactionInvocationHandler<D> implements InvocationHandler {
+
+        private final D dao;
+
+        private final String dataSourceJndiName;
+
+        private final String moduleName;
+
+        public TransactionInvocationHandler(D dao, String dataSourceJndiName, String moduleName) {
+            this.dao = dao;
+            this.dataSourceJndiName = dataSourceJndiName;
+            this.moduleName = moduleName;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Class<?> daoClass = dao.getClass();
+            Method implementingMethod = daoClass.getMethod(
+                    method.getName(), method.getParameterTypes());
+            return method.invoke(dao, args);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <D> D createProxy(D dao, String dataSourceJndiName, String moduleName) {
+        Class<?> daoClass = dao.getClass();
+        return (D) Proxy.newProxyInstance(
+                TransactionFactory.class.getClassLoader(),
+                daoClass.getInterfaces(),
+                new TransactionInvocationHandler<D>(dao, dataSourceJndiName, moduleName));
+    }
+}
+
