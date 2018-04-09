65ff842205e29c7f707c004eef7f061e4571753e
Vitalii Ivanov <vitr.ivanov@gmail.com>
2018-04-09 11:19:21 +0400

    add (JavaEE_122017): CDI

--- src/main/java/ru/otus/cdi/MessageCDIServlet.java
+++ src/main/java/ru/otus/cdi/MessageCDIServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "MessageCDIServlet", urlPatterns = "/messagecdi")
 public class MessageCDIServlet extends HttpServlet {
