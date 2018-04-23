fcbf9ba66ba5d69a2383bbe19be9949bd9698af2
ViTr <ViTr@list.ru>
2018-04-23 23:05:40 +0400

    add(JavaEE_122017): OWASP

--- src/main/java/ru/otus/ejb/aop/AuditEJB.java
+++ src/main/java/ru/otus/ejb/aop/AuditEJB.java
@@ -1,9 +1,11 @@
 package ru.otus.ejb.aop;
 
+import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
 
 @Stateless
 @Auditable
+@PermitAll
 public class AuditEJB {
 
     public String bizMethod(){
--- src/main/java/ru/otus/ejb/aop/Count.java
+++ src/main/java/ru/otus/ejb/aop/Count.java
@@ -1,21 +1,37 @@
 package ru.otus.ejb.aop;
 
+import javax.annotation.Resource;
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
+import javax.annotation.security.RunAs;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 import java.io.Serializable;
+import java.security.Principal;
 
 @Stateful
 @Interceptors({Logger.class/*, Logger.class*/})
+@DeclareRoles("MANAGER")
+@RunAs("ADMIN")
 public class Count implements Serializable {
+
+    @Resource
+    SessionContext context;
 
     int count = 1;
 
 //    @Interceptors(Logger.class)
     public int getResult() {
+        Principal principal = context.getCallerPrincipal();
+        if (context.isCallerInRole("ADMIN")) {
+
+        }
         return count;
     }
 
+    @RolesAllowed({"ADMIN", "MANAGER"})
     public void increment(){
         count++;
     }
